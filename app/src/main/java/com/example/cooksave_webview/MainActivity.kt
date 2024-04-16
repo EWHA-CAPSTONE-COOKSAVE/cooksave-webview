ad5dc71f721d9976721d48afa1474e56e70e192b
jimin song <songing01@gmail.com>
2024-04-16 14:45:46 +0900

    feat: 갤러리에서 사진 첨부 기능 구현

--- app/src/main/java/com/example/cooksave_webview/MainActivity.kt
+++ app/src/main/java/com/example/cooksave_webview/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.cooksave_webview
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    var mFilePathCallback: ValueCallback<Array<Uri>>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val myWebView: WebView = findViewById(R.id.webView)
+
 
 
         myWebView.settings.run {
@@ -28,7 +35,58 @@ class MainActivity : AppCompatActivity() {
         myWebView.loadUrl("https://www.cooksave.co.kr")
 
 
+        myWebView.webChromeClient = object : WebChromeClient() {
+            // For Android 5.0+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                // Ensure that there's no existing callback
+                mFilePathCallback = filePathCallback
+
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.setType("image/*")
+                // 파일 1개만 선택 가능하도록 처리
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+
+                resultLauncher.launch(intent)
+                return true
+            }
+
+        }
+
+
     }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val resultCode = result.resultCode
+            val intent = result.data
+
+            if (resultCode == Activity.RESULT_OK) {
+                val clipData = intent?.clipData
+                val results = mutableListOf<Uri>()
+
+                if (clipData != null) {
+                    for (i in 0 until clipData.itemCount) {
+                        val item = clipData.getItemAt(i)
+                        results.add(item.uri)
+                    }
+                } else {
+                    intent?.data?.let { results.add(it) }
+                }
+
+                mFilePathCallback?.onReceiveValue(results.toTypedArray())
+            } else {
+                mFilePathCallback?.onReceiveValue(null)
+            }
+
+            mFilePathCallback = null
+        }
+
+
 
     override fun onBackPressed() { // 뒤로가기 기능 구현
 
